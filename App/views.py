f64ad775d60eb36e96a8046a3821721978feb468
Dyuuz <lekanoyesunle@gmail.com>
2025-03-24 07:00:42 +0100

    removed trailing spaces

--- App/views.py
+++ App/views.py
@@ -10,14 +10,12 @@ from .models import Client
 class HomeView(View):
     template_name = 'layout.html'
 
-
     def get(self, request, *args, **kwargs):
 
         return render(request, self.template_name)
 
 class JobsAvailableView(View):
     template_name = 'JobsAvailable.html'
-
 
     def get(self, request, *args, **kwargs):
 
@@ -26,14 +24,12 @@ class JobsAvailableView(View):
 class JobsAppliedView(View):
     template_name = 'JobsApplied.html'
 
-
     def get(self, request, *args, **kwargs):
 
         return render(request, self.template_name)
 
 class DocumentView(View):
     template_name = 'Documents.html'
-
 
     def get(self, request, *args, **kwargs):
 
@@ -42,14 +38,12 @@ class DocumentView(View):
 class ProfileView(View):
     template_name = 'Profile.html'
 
-
     def get(self, request, *args, **kwargs):
 
         return render(request, self.template_name)
 
 class SettingsView(View):
     template_name = 'Settings.html'
-
 
     def get(self, request, *args, **kwargs):
 
